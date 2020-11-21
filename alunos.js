63d3686d94ed22ca57aead5ee89f20a20e978712
Daniela Luiza <danii.bnu@hotmail.com>
2020-11-21 14:31:45 -0300

    teste

--- alunos.js
+++ alunos.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
-        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
-    else //Se a requisição já é HTTPS 
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+app.use((req, res, next) => { 
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) 
+        res.redirect(`https://${req.headers.host}${req.url}`); 
+    else 
+        next(); 
 });
 
 app.get("/alunos", function(req, res){
-	var MongoClient = require('mongodb').MongoClient;
-	var url = "geonosis.mongodb.umbler.com:39472";
+	var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
 
-	MongoClient.connect(url, function(err, db) {
-  	if (err) throw err;
+	MongoClient.connect('geonosis.mongodb.umbler.com:39472', function(err, db) {
+    if(err) throw err;
+
   		var dbo = db.db("brocolisdb");
   		dbo.brocolisdb("brocolisdb").find({}).toArray(function(err, result) {
     	if (err) throw err;
--- hora.js
+++ hora.js
@@ -1,33 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
-        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
-    else //Se a requisição já é HTTPS 
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+app.use((req, res, next) => {  
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) 
+        res.redirect(`https://${req.headers.host}${req.url}`); 
+    else 
+        next(); 
 });
 
 app.get("/hora", function(req, res){
 	var data = new Date();
 
-// Guarda cada pedaço em uma variável
-	var dia     = data.getDate();           // 1-31
-	var dia_sem = data.getDay();            // 0-6 (zero=domingo)
-	var mes     = data.getMonth();          // 0-11 (zero=janeiro)
-	var ano2    = data.getYear();           // 2 dígitos
-	var ano4    = data.getFullYear();       // 4 dígitos
-	var hora    = data.getHours();          // 0-23
-	var min     = data.getMinutes();        // 0-59
-	var seg     = data.getSeconds();        // 0-59
-	var mseg    = data.getMilliseconds();   // 0-999
-	var tz      = data.getTimezoneOffset(); // em minutos
+	var dia     = data.getDate();           
+	var dia_sem = data.getDay();            
+	var mes     = data.getMonth();         
+	var ano2    = data.getYear();           
+	var ano4    = data.getFullYear();       
+	var hora    = data.getHours();          
+	var min     = data.getMinutes();        
+	var seg     = data.getSeconds();        
+	var mseg    = data.getMilliseconds();   
+	var tz      = data.getTimezoneOffset(); 
 
-// Formata a data e a hora (note o mês + 1)
 	var str_data = dia + '/' + (mes+1) + '/' + ano4;
 	var str_hora = hora + ':' + min + ':' + seg;
 
-// Mostra o resultado
 	res.send('Hoje é ' + str_data + ' às ' + str_hora);
 })
 
